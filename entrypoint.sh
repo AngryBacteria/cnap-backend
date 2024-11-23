f62b534987b3866b958ae9067b959f9050e820ac
AngryBacteria <AngryBacteria@protonmail.com>
2024-11-23 17:55:12 +0100

    new correct entrypoint for task

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$RUN_TYPE" = "task" ]; then
     echo "Running Periodic Updates"
-    python tasks/PeriodicUpdates.py
+    python tasks/MainTask.py
 else
     echo "Starting FastAPI Backend"
     fastapi run api/main.py
