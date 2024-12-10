8fa7c3ab104b61c0ebd60dcbb5e4e63e51d1a1f1
AngryBacteria <AngryBacteria@protonmail.com>
2024-12-10 17:37:15 +0100

    docker compose

--- tasks/MainTask.py
+++ tasks/MainTask.py
@@ -14,6 +14,10 @@ async def interval_update(iteration, interval_time):
         await summoner_tasks.update_summoner_data()
         await game_data_task.update_items()
         await game_data_task.update_champions()
+        await game_data_task.update_game_modes()
+        await game_data_task.update_game_types()
+        await game_data_task.update_maps()
+        await game_data_task.update_queues()
     await match_tasks.update_match_data(69, 0)
     await asyncio.sleep(interval_time)
 
